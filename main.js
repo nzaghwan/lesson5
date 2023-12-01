d0079db6362b53b574f47f8bcce2f29e38f46c66
nzaghwan <nzaghwan@gmail.com>
2023-12-01 10:54:44 -0600

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+const sendData = () => {
+    localStorage.setItem("Naser", "Zaghwan"); 
+}
+
+// showData.innerText = localStorage.getItem("Naser");  
+
+// let lastname = localStorage.getItem(key);
+const getData = () => {
+  showData.innerText = localStorage.getItem("Naser");  
+}
